43536f93ade7a325ff1f8b0b906f7243ba6a3867
MedUnes <contact@medunes.net>
2017-12-08 20:49:28 +0100

    removed irrelevant code

--- models/likes.js
+++ models/likes.js
@@ -56,36 +56,7 @@ class Likes{
 			}			
 		});	
 	}
-	static findOrCreate(keyName,Likes,callback){
-		keyName = Likes.keyNames().includes(keyName)?keyName:"id";	
-		// Get all messages from DB
-		connection.query(`SELECT * FROM \`Likes\` WHERE ${keyName} = ?`,[Likes[keyName]],(error,results,fields)=>{
-			if(error) console.log(error);
-			
-			// If the Likes was found
-			if(results.length>0){
-				let row = results[0];
-				// set the found Likes as a paramater to the callback function
-				callback(new Likes(row));
-			} else {
-				// create a new Likes and set it as a paramater to the callback function
- 				Likes.create(Likes,callback);
-			}			
-		});
-	}
-	static update(Likes,callback){
-		if(Likes.photo){
-			connection.query(`UPDATE \`Likes\` SET firstName = ?, lastName = ?, photo = ? WHERE id =?`,[Likes.firstName,Likes.lastName,Likes.photo.filename,Likes.id],(error,results,fields)=>{
-				if(error) console.log(error);
-				Likes.find(results.updatedId,callback);
-			});
-		} else {
-			connection.query(`UPDATE \`Likes\` SET firstName = ?, lastName = ? WHERE id =?`,[Likes.firstName,Likes.lastName,Likes.id],(error,results,fields)=>{
-				if(error) console.log(error);
-				Likes.find(results.updatedId,callback);
-			});
-		}
-	}
+	
 }
 
 // Export the Likes class
